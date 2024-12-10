ffd4ee4af30ff011cc1100ef017bef651cf2921a
Sphynxxxxx <larrydenverbiaco@gmail.com>
2024-12-11 03:19:05 +0800

    aaaaaaaaaa

--- ApprovedLend.php
+++ ApprovedLend.php
@@ -17,7 +17,7 @@ if (!$result) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Lender's Profile</title>
+    <title>No Profile</title>
     <link rel="stylesheet" href="styles.css"> <!-- Optional for styling -->
 </head>
 <body>
